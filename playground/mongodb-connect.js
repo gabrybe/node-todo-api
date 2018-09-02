ccc989e3dcf87b143292dab59b005bb18143a386
Gabriele Bernuzzi <gabriele.bernuzzi@tesisquare.com>
2018-09-02 21:51:02 +0200

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,49 @@
+﻿// const MongoClient = require("mongodb").MongoClient;
+// const ObjectID = require("mongodb").ObjectID
+
+// stesse operazioni di cui sopra, ma con object destructuring di es6
+const {MongoClient, ObjectID} = require("mongodb");
+
+// creo un nuovo object id (non serve necessariamente solo con mongodb)
+var objId = new ObjectID();
+console.log(objId);
+
+// url del db, opzioni, callback da eseguire quando la connessione è avvenuta con successo (oppure no)
+MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true}, (err, client) => {
+  if (err) {
+    return console.log("Unable to connect to MongoDB server", err);
+  }
+
+  console.log("Connected to MongoDB server");
+  const db = client.db("TodoApp");
+
+  // mongodb crea in automatico le collection se non esistono
+  // insertOne(object, callback)
+  db.collection("Todos").insertOne({
+    text: "Something to do - another note",
+    completed: false
+  }, (err, result) => {
+    if (err) {
+      return console.log("Unable to insert in Todos collection", err)
+    }
+    // documents inseriti: in result.ops
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  db.collection("Users").insertOne({
+    name: "Gabry", age: 38, location: "Turin"
+  }, (err, result) => {
+    if (err) {
+      return console.log("Unable to insert in Users collection", err);
+    }
+
+    // documents inseriti: in result.ops
+    console.log(JSON.stringify(result.ops, undefined, 2));
+
+    // quando è stato creato questo document?
+    // l'object id contiene questa informazione
+    console.log("Creation date: ", result.ops[0]._id.getTimestamp());
+  });
+
+  client.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,45 @@
+﻿// const MongoClient = require("mongodb").MongoClient;
+// const ObjectID = require("mongodb").ObjectID
+
+// stesse operazioni di cui sopra, ma con object destructuring di es6
+const {MongoClient, ObjectID} = require("mongodb");
+
+// creo un nuovo object id (non serve necessariamente solo con mongodb)
+var objId = new ObjectID();
+console.log(objId);
+
+// url del db, opzioni, callback da eseguire quando la connessione è avvenuta con successo (oppure no)
+MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true}, (err, client) => {
+  if (err) {
+    return console.log("Unable to connect to MongoDB server", err);
+  }
+
+  console.log("Connected to MongoDB server");
+  const db = client.db("TodoApp");
+
+  // mongodb crea in automatico le collection se non esistono
+  // toArray() restitusce una Promise
+  db.collection("Todos").find({completed: false}).toArray().then((docs) => {
+    console.log("Todos", JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log("Unable to fetch todos", err)
+  });
+
+  // per leggere un object id specifico bisogna passare da ObjectID(), non da una stringa
+  db.collection("Todos").find({
+    _id: new ObjectID("5b8bd7a4b853261efc4648e8")
+  }).toArray().then((docs) => {
+    console.log("Todos", JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log("Unable to fetch todos", err)
+  });
+
+  // conteggio dei record
+  db.collection("Users").find().count().then((count) => {
+    console.log(count);
+  }, (err) => {
+    console.log(err)
+  });
+
+  client.close();
+});
