ef63e0b262e521bded1fbe0a8216411f489f0c6d
Gabriele Bernuzzi <gabriele.bernuzzi@tesisquare.com>
2018-09-07 11:51:47 +0200

    Heroku setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,8 @@
 
 // bisogna indicare a mongoose quale libreria di Promises si utilizza: usiamo quella builtin
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/TodoApp", {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp", {useNewUrlParser: true});
 
 module.exports = {
   mongoose: mongoose
